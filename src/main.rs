636d32ba29f61726ee1c24ee53972ef412cc0da2
Maddy Guthridge <hello@maddyguthridge.com>
2025-09-25 23:03:36 +1000

    Load base settings file from parameter if needed

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@
 //! A configuration tool to share VS Code settings more easily.
 //!
 //! This is main.rs, the main entrypoint to the program.
-mod loader;
 mod apply;
+mod loader;
 
 use std::{env, fs, path::PathBuf};
 
@@ -16,25 +16,34 @@ use crate::{apply::apply, loader::load_snippet};
 /// Returns the path to the user settings JSON file
 ///
 /// https://code.visualstudio.com/docs/configure/settings#_user-settingsjson-location
-fn code_settings_file() -> PathBuf {
-    if cfg!(target_os="windows") {
+fn code_settings_file() -> String {
+    if cfg!(target_os = "windows") {
         PathBuf::from(env::var("APPDATA").unwrap())
             .join("Code")
             .join("User")
             .join("settings.json")
-    } else if cfg!(target_os="linux") {
+            .to_str()
+            .unwrap()
+            .to_owned()
+    } else if cfg!(target_os = "linux") {
         PathBuf::from(env::var("HOME").unwrap())
             .join(".config")
             .join("Code")
             .join("User")
             .join("settings.json")
-    } else if cfg!(target_os="macos") {
+            .to_str()
+            .unwrap()
+            .to_owned()
+    } else if cfg!(target_os = "macos") {
         PathBuf::from(env::var("HOME").unwrap())
             .join("Library")
             .join("Application Support")
             .join("Code")
             .join("User")
             .join("settings.json")
+            .to_str()
+            .unwrap()
+            .to_owned()
     } else {
         panic!("Unknown platform, cannot determine Code settings file path")
     }
@@ -44,7 +53,7 @@ fn code_settings_file() -> PathBuf {
 #[command(version, about)]
 struct Args {
     /// The URLs or file paths for settings to apply
-    #[arg(required=true)]
+    #[arg(required = true)]
     snippets: Vec<String>,
 
     /// The VS Code settings file to modify
@@ -54,28 +63,29 @@ struct Args {
     /// Whether to accept installing all of the given settings automatically,
     /// as opposed to prompting for every snippet set.
     #[arg(long)]
-    all: bool
+    all: bool,
 }
 
 fn main() {
     let cli = Args::parse();
 
     // Holy moly I love Rust's type system so much!
-    let snippets = cli.snippets
+    let snippets = cli
+        .snippets
         .iter()
         .map(|snip| load_snippet(snip))
         .collect::<Result<Vec<String>, _>>()
         .unwrap();
 
-        // Load user's base settings
-    let base_settings = String::try_from(fs::read(code_settings_file()).unwrap()).unwrap();
+    // Load user's base settings
+    let base_settings =
+        String::try_from(fs::read(cli.file.unwrap_or_else(|| code_settings_file())).unwrap())
+            .unwrap();
 
     // Now apply all snippets
-    let new_settings = snippets
-        .into_iter()
-        .fold(base_settings, |settings, snip| apply(&settings, &snip).unwrap());
+    let new_settings = snippets.into_iter().fold(base_settings, |settings, snip| {
+        apply(&settings, &snip).unwrap()
+    });
 
     println!("{new_settings}");
-
-
 }
